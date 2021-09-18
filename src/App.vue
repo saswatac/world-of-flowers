5eed0aa0a2247731d66c0223a83861110dbabdab
Saswata Chakravarty <saswata.chakravarty@neuralconcept.com>
2021-09-18 12:27:55 +0200

    use leaflet maps

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,26 @@
 <template>
   <div>
-    <carousel-3d ref=mycarousel :controls-visible="true" @after-slide-change="onAfterSlideChange">
+    <carousel-3d ref=mycarousel :controls-visible="true" @after-slide-change="onAfterSlideChange" :display="1">
       <slide v-for="(image, i) in imgs" v-bind:key="i" :index=i>
         <div @click="() => showSingle(i)">
-          <img :src=imgs[i].url alt="Italian Trulli">
+          <img :src=imgs[i].url alt="Italian Trulli" loading=lazy>
         </div>
       </slide>
     </carousel-3d>
-    <GmapMap
-      :center='center'
-      :zoom='12'
-      style='width:100%;  height: 400px;'
+    <l-map ref="myMap"> </l-map>
+    <l-map
+      :zoom=13
+      :center="center"
+      :options="mapOptions"
+      style="height: 850px; width: 850px"
     >
-      <GmapMarker
-        :key="index"
-        v-for="(m, index) in imgs"
-        :position="imgs[index].location"
-        :clickable="true"
-        @click="() => goToSlide(index)"
+      <l-tile-layer
+        :url="url"
+        :attribution="attribution"
       />
-    </GmapMap>
+      <l-marker v-for="(image, i) in imgs" v-bind:key="i" :lat-lng="imgs[i].location" :opacity=0.5 v-on:click="() => goToSlide(i)">
+      </l-marker>
+    </l-map>
     <!-- all props & events -->
     <vue-easy-lightbox
       escDisabled
@@ -36,46 +37,59 @@
   // If VueApp is already registered with VueEasyLightbox, there is no need to register it here.
   import VueEasyLightbox from 'vue-easy-lightbox';
   import { Carousel3d, Slide } from 'vue-carousel-3d';
+  //import L from 'leaflet';
+  import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
 
   export default {
     components: {
       VueEasyLightbox,
       Carousel3d,
       Slide,
+      LMap,
+      LMarker,
+      LTileLayer,
+      //LPopup,
+      //LTooltip
     },
     data() {
       return {
         imgs: [
-            { url: "https://images.unsplash.com/photo-1628191012047-e789922abfdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1650&q=80",
-              location: { lat: 47.508, lng: 6.587 }
+            { url: "http://localhost:8000/PXL_20210905_112008235.MP.jpg",
+              location: [47.508, 6.587]
             },
             { url: "http://localhost:8000/PXL_20210905_121021611.jpg",
-              location: { lat: 46.508, lng: 6.587 }
+              location: [46.508, 6.587]
             },
             { url: "http://localhost:8000/PXL_20210905_121021611.jpg",
-              location: { lat: 46.508, lng: 6.587 }
+              location: [46.508, 6.587]
             },
             { url: "http://localhost:8000/PXL_20210905_121021611.jpg",
-              location: { lat: 46.508, lng: 6.587 }
+              location: [46.508, 6.587]
             },
             { url: "http://localhost:8000/PXL_20210905_121021611.jpg",
-              location: { lat: 46.508, lng: 6.587 }
+              location: [46.508, 6.587]
             },
             { url: "http://localhost:8000/PXL_20210905_121021611.jpg",
-              location: { lat: 46.508, lng: 6.587 }
+              location: [46.508, 6.587]
             },
             { url: "http://localhost:8000/PXL_20210905_121021611.jpg",
-              location: { lat: 46.508, lng: 6.587 }
+              location: [46.508, 6.587]
             },
             { url: "http://localhost:8000/PXL_20210905_121021611.jpg",
-              location: { lat: 46.508, lng: 6.587 }
+              location: [46.508, 6.587]
             }
 
         ], // Img Url , string or Array of string
         visible: false,
         index: 0, // default: 0
         selected_img: '',
-        center: { lat: 46.508, lng: 6.587 },
+        center: [46.508, 6.587],
+        mapOptions: {
+          zoomSnap: 0.5
+        },
+        url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+
       }
     },
     methods: {
